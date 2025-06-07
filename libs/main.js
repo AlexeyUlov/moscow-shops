22fdf890066f0965e0fac4293c9b1445bcc120f9
petergriffinfunnymoments <petergriffinemailmoment@gmail.com>
2025-06-07 21:45:41 +0300

    Update main.js
    яичный маньяк

--- libs/main.js
+++ libs/main.js
@@ -70,16 +70,34 @@ createMarker(55.7440113, 37.5671697, 'Levis (Пл. Киевского вокза
 
 // Реселлеры
 createMarker(55.759500, 37.664635, 'Spin4Spin',
-    '<b>Название:</b> Spin4Spin<br><b>Адрес: </b>Нижний Сусальный пер., 5, стр. 17, Москва, этаж цокольный<b>Представленные бренды: </b>Balenciaga, Rick Owens, Maison Margiela<br><b>Контакты: </b>+7(495) 127-78-95, jnsonline.ru<br><img src="data/logos/jns_logo.png" width="100px">',
+    '<b>Название:</b> Spin4Spin<br><b>Адрес: </b>Нижний Сусальный пер., 5, стр. 17, Москва, этаж цокольный<b><br>Представленные бренды: </b>Balenciaga, Rick Owens, Maison Margiela<br><b>Тип: </b>Комиссионный магазин<br><b>Контакты: </b>-<br><b>Режим работы: </b>Ежедневно, 12:00 - 21:00<br>',
+    parkIconArray[1], 'ушли');
+createMarker(55.756683, 37.642499, 'Cab',
+    '<b>Название:</b> SV77<br><b>Адрес: </b>Подкопаевский пер., 2/6с2, Москва<b><br>Представленные бренды: </b>Balenciaga, Rick Owens<br><b>Тип: </b>Магазин одежды<br><b>Контакты: </b>+7 (977) 197-90-21, https://t.me/CABstoree<br><b>Режим работы: </b>Ежедневно, 11:00 - 22:00<br>',
+    parkIconArray[1], 'ушли');
+createMarker(55.764198, 37.635196, 'Second Friend Store',
+    '<b>Название:</b> SV77<br><b>Адрес: </b>ул. Мясницкая 24/7, стр. 1, под. 12,второй этаж, код домофона 0000м. Чистые пруды, г. Москва<b><br>Представленные бренды: </b>Balenciaga, Fendi, Maison Margiela<br><b>Тип: </b>Секонд-хенд<br><b>Контакты: </b>8 (800) 777-61-49, secondfriendstore.ru<br><b>Режим работы: </b>Ежедневно, 11:00 - 21:00<br>',
+    parkIconArray[1], 'ушли');
+createMarker(55.764198, 37.635196, 'Second Friend Store',
+    '<b>Название:</b> SV77<br><b>Адрес: </b>ул. Мясницкая 24/7, стр. 1, под. 12,второй этаж, код домофона 0000м. Чистые пруды, г. Москва<b><br>Представленные бренды: </b>Balenciaga, Fendi, Maison Margiela<br><b>Тип: </b>Секонд-хенд<br><b>Контакты: </b>8 (800) 777-61-49, secondfriendstore.ru<br><b>Режим работы: </b>Ежедневно, 11:00 - 21:00<br>',
+    parkIconArray[1], 'ушли');
+createMarker(55.761976, 37.612066, 'KM20',
+    '<b>Название:</b> KM20<br><b>Адрес: </b>Столешников пер., 2, стр. 1, Москва<b><br>Представленные бренды: </b>Jean Paul Gaultier, Maison Margiela, Maison MIHARA YASUHIRO<br><b>Тип: </b>Магазин одежды<br><b>Контакты: </b>-<br><b>Режим работы: </b>Ежедневно, 11:00 - 23:00<br>',
+    parkIconArray[1], 'ушли');
+createMarker(55.761976, 37.612066, 'Wayoff',
+    '<b>Название:</b> Wayoff<br><b>Адрес: </b>Тверская ул., 19, Москва<b><br>Представленные бренды: </b>Balenciaga, Burberry, Dior<br><b>Тип: </b>Магазин одежды<br><b>Контакты: </b>8 (800) 333-88-39, wayoff.ru, https://vk.com/wayoff<br><b>Режим работы: </b>Ежедневно, 10:00 - 22:00<br>',
+    parkIconArray[1], 'ушли');
+createMarker(55.784959, 37.583303, 'Anothershop',
+    '<b>Название:</b> Anothershop<br><b>Адрес: </b>3-я ул. Ямского Поля, 2, корп. 25, Москва<b><br>Представленные бренды: </b>Maison Margiela, Balenciaga, Burberry<br><b>Тип: </b>Магазин одежды<br><b>Контакты: </b>+7 (499) 394-63-35, www.anothershop.net<br>https://t.me/anothershop<br>https://vk.com/anothershop<br><b>Режим работы: </b>Ежедневно, 13:00 - 21:00<br>',
+    parkIconArray[1], 'ушли');
+createMarker(55.835683, 37.658259, 'Poizon Shop',
+    '<b>Название:</b> Poizon Shop<br><b>Адрес: </b>просп. Мира, 188Б, корп. 3, Москва<b><br>Представленные бренды: </b>Nike, The North Face, New Balance<br><b>Тип: </b>Шоурум<br><b>Контакты: </b>-<br><b>Режим работы: </b>Ежедневно, 13:00 - 22:00<br>',
     parkIconArray[1], 'ушли');
 
-// Пример реселлера:
-createMarker(55.75, 37.61, 'Реселлер XYZ',
-    'Пример реселлера', parkIconArray[1], 'реселлеры');
-
 // Пример бутика:
-createMarker(55.74, 37.63, 'Бутик ABC',
-    'Пример бутика', parkIconArray[2], 'бутики');
+createMarker(55.760614, 37.600657, 'SV77',
+    '<b>Название:</b> SV77<br><b>Адрес: </b>Тверской бул., 14, стр. 3<b><br>Представленные бренды: </b>Balenciaga, Diesel, Maison Margiela<br><b>Тип: </b>Бутик<br><b>Контакты: </b>-<br><b>Режим работы: </b>Ежедневно, 11:00 - 22:00<br>',
+    parkIconArray[2], 'ушли');
 
 // Пример секонд хенда:
 createMarker(55.76, 37.66, 'Секонд Хенд №1',
